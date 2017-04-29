4d6ba521e5c491d22b3c0868618a74e1419a6046
velascoDev <dracontium2@gmail.com>
2017-04-28 19:07:44 -0500

    Implemented React life cycle methods

--- source/components/StreamTweet.react.js
+++ source/components/StreamTweet.react.js
@@ -11,6 +11,27 @@ const StreamTweet = React.createClass({
 			headerText: null
 		};
 	},
+	componentWillMount() {
+		console.log('[Tweecs] StreamTweet: 2. Running componentWillMount()');
+		this.setState({
+			numberOfCharactersIsIncreasing: true,
+			headerText: 'Latest public photo from Twitter'
+		});
+		//remove in production - demo purposes only
+		window.tweecs = {
+			numberOfReceivedTweets: 1,
+			numberOfDisplayedTweets: 1
+		};
+	},
+	componentDidMount() {
+		console.log('[Tweecs] StreamTweet: 3. Running componentDidMount()');
+		let componentDOMRepresentation = ReactDOM.findDOMNode(this);
+		window.tweecs.headerHtml = componentDOMRepresentation.children[0].outerHTML;
+		window.snapterest.tweetHtml = componentDOMRepresentation.children[1].outerHTML;
+	},
+	componentWillUnmount() {
+		delete window.tweecs;
+	}
 	render(){
 		console.log('[Tweecs] StreamTweet: Running render()');
 		return{
